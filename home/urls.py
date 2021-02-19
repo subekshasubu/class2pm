3718db2c85331334b5fb2f7f0650448c961c3b8f
subekshasubu <subeksha30@gmail.com>
2021-02-19 22:58:13 +0545

    header files for header and footer

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from .views import *
 app_name = 'home'
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', service, name='services'),
 ]
